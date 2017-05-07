f044ebe59438e0007995b1f65e27136705f99201
Cyrus Zei <cyruszei@gmail.com>
2017-05-07 20:49:28 +0200

    added nav

--- src/sections/header/nav/nav.js
+++ src/sections/header/nav/nav.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
-
+import Logo from './../../../assets/images/toughest-logo.png'
 
 class Nav extends Component {
   render() {
     return (
-      <div className='row black'>
+      <div className='row black nav'>
         <div className='block _25'>
-          logo here
+          <img src={Logo}/>
         </div>
         <div className='block _75'>
-          Meny here
+          <ul className='nav_ul'>
+            <li><a href="#">ANMÄLAN</a></li>
+            <li><a href="#">RACE</a></li>
+            <li><a href="#">INFO</a></li>
+            <li><a href="#">MEDIA</a></li>
+            <li><a href="#">SHOP</a></li>
+            <li><a href="#">KONTAKT</a></li>
+
+          </ul>
         </div>
 
 
