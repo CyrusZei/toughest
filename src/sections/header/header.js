a5cd02defa60c9f651f7d4b41453fa9c78169e74
Cyrus Zei <cyruszei@gmail.com>
2017-05-07 20:25:08 +0200

    added header and nav

---
+++ src/sections/header/header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Nav from './nav/nav'
+
+class Header extends Component {
+  render () {
+    return (
+      <div>
+        <Nav />
+      </div>
+
+    );
+  }
+}
+
+
+export default Header;
---
+++ src/sections/header/nav/nav.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+
+class Nav extends Component {
+  render() {
+    return (
+      <div className='row black'>
+        <div className='block _25'>
+          logo here
+        </div>
+        <div className='block _75'>
+          Meny here
+        </div>
+
+
+      </div>
+    );
+  }
+}
+
+export default Nav;
