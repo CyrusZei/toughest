d0505da44d3b4c80af8898487b3c0e9696de4687
Cyrus Zei <cyruszei@gmail.com>
2017-05-08 20:00:06 +0200

    fixed signed up section

--- src/sections/miniSections/signedup/signedup.js
+++ src/sections/miniSections/signedup/signedup.js
@@ -6,15 +6,24 @@ class SignedUp extends Component {
     return (
       <div className='row black'>
         <div className='block _33  color display_flex'>
-          <p>01</p>
+          <div>
+            <p className='signed_up_title'>Signed Up</p>
+            <p className='signed_up_sub'>2345</p>
+          </div>
         </div>
 
         <div className='block _33  color display_flex'>
-          <p>01</p>
+          <div>
+            <p className='signed_up_title'>Best Time</p>
+            <p className='signed_up_sub'>4H : 13min</p>
+          </div>
         </div>
 
         <div className='block _33  color display_flex'>
-          <p>01</p>
+          <div>
+            <p className='signed_up_title'>Average Age</p>
+            <p className='signed_up_sub'>32 Years</p>
+          </div>
         </div>
 
 
