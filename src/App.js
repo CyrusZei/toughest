4d6d93c37995b98d68f09bcb4f8edfe9dda18670
Cyrus Zei <cyruszei@gmail.com>
2017-05-08 19:49:59 +0200

    fixed grid

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import ProveIt from './sections/miniSections/proveit/proveit';
 import ObsMap from './sections/obsMap/obsMap';
 import BeaLegend from './sections/miniSections/beaLegend/beaLegend';
 import Gallery from './sections/gallery/gallery';
+import SignedUp from './sections/miniSections/signedup/signedup';
+
+
 
 class App extends Component {
   render() {
---
+++ src/sections/miniSections/signedup/signedup.js
@@ -0,0 +1,26 @@
+import React , { Component } from 'react';
+
+
+class SignedUp extends Component {
+  render() {
+    return (
+      <div className='row black'>
+        <div className='block _33  color display_flex'>
+          <p>01</p>
+        </div>
+        
+        <div className='block _33  color display_flex'>
+          <p>01</p>
+        </div>
+
+        <div className='block _33  color display_flex'>
+          <p>01</p>
+        </div>
+
+
+      </div>
+    );
+  }
+}
+
+export default SignedUp;
