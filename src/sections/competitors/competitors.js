f7a8c3771926951fc2043799515f47b1bb972834
Cyrus Zei <cyruszei@gmail.com>
2017-05-08 20:58:22 +0200

    xhr error, sent email to mattias about this

--- src/sections/competitors/competitors.js
+++ src/sections/competitors/competitors.js
@@ -1,33 +1,69 @@
 import React , { Component } from 'react';
-
+import axios from 'axios';
 
 
 class Competitors extends Component {
+
+  componentWillMount() {
+
+
+
+    axios({
+      method:'get',
+      url:'http://api.notified.com/Channel/66bbc5cf-0030-e711-80d6-a0d3c1f2861f?key=ab07bd54-7766-46dd-8480-2daa14035af0&format=basic&limit=50',
+      headers: '',
+      responseType: 'xml',
+
+    })
+    .then((snap) => {
+      console.log(snap.headers);
+    });
+
+
+  }
+
   render() {
   return(
     <div>
-    <div className='row grey'>
-      <div className='block color display_flex'>
-        <div>
-          <p className='big_title'>Competitors</p>
-          <p className='big_title_sub'>What they say</p>
+
+      <div className='row grey'>
+        <div className='block color display_flex'>
+          <div>
+            <p className='big_title'>Competitors</p>
+            <p className='big_title_sub'>What they say</p>
+          </div>
         </div>
       </div>
-    </div>
-    <div className='row grey'>
-      <div className='block _50 color display_flex'>
-        <div>
-          <p className='big_title'>Competitors</p>
-          <p className='big_title_sub'>What they say</p>
+
+      <div className='row grey'>
+        <div className='block _50 color display_flex'>
+          <div>
+            <p className='big_title'>Competitors</p>
+            <p className='big_title_sub'>What they say</p>
+          </div>
+        </div>
+        <div className='block _50 color display_flex'>
+          <div>
+            <p className='big_title'>Competitors</p>
+            <p className='big_title_sub'>What they say</p>
+          </div>
         </div>
       </div>
-      <div className='block _50 color display_flex'>
-        <div>
-          <p className='big_title'>Competitors</p>
-          <p className='big_title_sub'>What they say</p>
+
+      <div className='row grey'>
+        <div className='block _50 color display_flex'>
+          <div>
+            <p className='big_title'>Competitors</p>
+            <p className='big_title_sub'>What they say</p>
+          </div>
+        </div>
+        <div className='block _50 color display_flex'>
+          <div>
+            <p className='big_title'>Competitors</p>
+            <p className='big_title_sub'>What they say</p>
+          </div>
         </div>
       </div>
-    </div>
 
     </div>
   );
