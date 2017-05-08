f8a6d5f0d5e5631f7fc4d24ee71faa9c95b532b3
Cyrus Zei <cyruszei@gmail.com>
2017-05-08 17:06:55 +0200

    fixed center center text

--- src/sections/miniSections/proveit/proveit.js
+++ src/sections/miniSections/proveit/proveit.js
@@ -5,15 +5,18 @@ class ProveIt extends Component {
   render() {
     return(
       <div className='row black'>
-        <div className='block _25 color'>
+        <div className='block _25 color display_flex'>
           <p>Malmö</p>
         </div>
-        <div className='block _50 color'>
-          <p>Prove it</p>
-          <p>Sign up</p>
+        <div className='block _50 color display_flex'>
+          <div>
+            <p>Prove it</p>
+            <p>Sign up</p>
+          </div>
+
         </div>
-        <div className='block _25 color'>
-          29 Augusti
+        <div className='block _25 color display_flex'>
+          <p>29 Augusti</p>
         </div>
 
       </div>
